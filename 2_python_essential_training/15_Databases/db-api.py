b3c5df34289700593014634b7a2e3400b1518c52
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-05-30 19:58:32 +0100

    Hmm..unsure of chap15 database_api of python3_essential_training

--- 2_python_essential_training/15_Databases/db-api.py
+++ 2_python_essential_training/15_Databases/db-api.py
@@ -1,43 +1,52 @@
 #!/usr/bin/env python3
-# Copyright 2009-2017 BHG http://bw.org/
-
 import sqlite3
 
+
 def main():
-    print('connect')
-    db = sqlite3.connect('db-api.db')
+    print("connect")
+    db = sqlite3.connect("db-api.db")
     cur = db.cursor()
-    print('create')
+    print("create")
     cur.execute("DROP TABLE IF EXISTS test")
-    cur.execute("""
+    cur.execute(
+        """
         CREATE TABLE test (
             id INTEGER PRIMARY KEY, string TEXT, number INTEGER
         )
-        """)
-    print('insert row')
-    cur.execute("""
+        """
+    )
+    print("insert row")
+    cur.execute(
+        """
         INSERT INTO test (string, number) VALUES ('one', 1)
-        """)
-    print('insert row')
-    cur.execute("""
+        """
+    )
+    print("insert row")
+    cur.execute(
+        """
         INSERT INTO test (string, number) VALUES ('two', 2)
-        """)
-    print('insert row')
-    cur.execute("""
+        """
+    )
+    print("insert row")
+    cur.execute(
+        """
         INSERT INTO test (string, number) VALUES ('three', 3)
-        """)
-    print('commit')
+        """
+    )
+    print("commit")
     db.commit()
-    print('count')
+    print("count")
     cur.execute("SELECT COUNT(*) FROM test")
     count = cur.fetchone()[0]
-    print(f'there are {count} rows in the table.')
-    print('read')
+    print(f"there are {count} rows in the table.")
+    print("read")
     for row in cur.execute("SELECT * FROM test"):
         print(row)
-    print('drop')
+    print("drop")
     cur.execute("DROP TABLE test")
-    print('close')
+    print("close")
     db.close()
 
-if __name__ == '__main__': main()
+
+if __name__ == "__main__":
+    main()
