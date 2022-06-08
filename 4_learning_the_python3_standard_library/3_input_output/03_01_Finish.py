3da747f8758efb0d4def2634dc9899157f8bbee5
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-06-08 22:03:17 +0100

    finished until basic file i/o Ch03 of python standard library: i/o

--- 4_learning_the_python3_standard_library/3_input_output/03_01_Finish.py
+++ 4_learning_the_python3_standard_library/3_input_output/03_01_Finish.py
@@ -2,22 +2,23 @@
 import sys
 
 # Print Arguments
-print("Number of arguments: ", len(sys.argv), ' arguments.')
+print("Number of arguments: ", len(sys.argv), " arguments.")
 print("Arguments ", sys.argv)
 
 # Remove Arguments
-sys.argv.remove(sys.argv[0])
+# sys.argv.remove(sys.argv[0])
+# sys.argv.pop(0)
 
-print("Arguments", sys.argv)
+# print("Arguments", sys.argv)
 
 # Sum up the Arguments
-arguments = sys.argv
-sum = 0
+arguments = sys.argv[1:]
+total = 0
 for arg in arguments:
     try:
         number = int(arg)
-        sum = sum + number
+        total += number
     except Exception:
         print("Bad input")
 
-print(sum)
+print(f"Sum is {total}")
--- 4_learning_the_python3_standard_library/3_input_output/03_03_Finish.py
+++ 4_learning_the_python3_standard_library/3_input_output/03_03_Finish.py
@@ -1,5 +1,4 @@
 # Files and File Writing
-
 # Open a file
 myFile = open("scores.txt", "w")
 
@@ -12,10 +11,12 @@ print("Name " + myFile.name)
 print("Mode " + myFile.mode)
 
 # Write to a file
-myFile.write("GBJ : 100\nKHD : 99\nBBB : 89")
+myFile.write("GBJ : 100\nKHD : 99\nABB : 89")
 myFile.close()
 
 # Read the file
 myFile = open("scores.txt", "r")
+# print("Reading..." + myFile.read())
 print("Reading..." + myFile.read(10))
 print("Reading again" + myFile.read(10))
+myFile.close()
