078edb3c34d0016ccda348330ba68773d8a81e2a
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-06-12 23:09:12 +0100

    moves 05 python for data science tips tricks and techniques into z_other folder

--- z_other/03_python_for_data_science_tips_tricks_and_techniques/01_01.py
+++ z_other/03_python_for_data_science_tips_tricks_and_techniques/01_01.py
@@ -23,16 +23,16 @@ from pprint import pprint as pp
 pp(d)
 
 # print keys at the top level
-print d.keys()
+print(d.keys())
 
 # print keys at the second level
 for a in d['contents']:
-    print a.keys()
+    print(a.keys())
 
 # print keys at the third level
 for a in d['contents']:
     for b in a['monthlySales']:
-        print b.keys()
+        print(b.keys())
 
 # print the keys and values at the first level
 for key, value in d.items():
@@ -41,7 +41,7 @@ for key, value in d.items():
 # print the keys and values at the second level
 for a in d['contents']:
     for key, value in a.items():
-        print key + ': ', value    
+        print key + ': ', value
 
 # print the keys and values at the third level
 for a in d['contents']:
