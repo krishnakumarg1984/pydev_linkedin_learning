30a4b23c3523a873c33fabb4da16db9bb41404c9
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-10-12 14:20:22 +0100

    adds some list of static analysis tools

--- 02_python_essential_training/12_File_IO/hello.py
+++ 02_python_essential_training/12_File_IO/hello.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-def main():
-    print('Hello, World.')
 
-if __name__ == '__main__': main()
+def main():
+    print("Hello, World.")
+
+
+if __name__ == "__main__":
+    main()
